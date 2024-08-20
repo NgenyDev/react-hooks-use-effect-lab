a67e33a4bcb34f2ce27830c5bcadd9611c65229a
Ngeny Gilbert <ngenygkip@gmail.com>
2024-08-20 08:13:51 +0300

    rect use effect

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,3 @@
-import React, { useState } from "react";
-import Question from "./Question";
-import quiz from "../data/quiz";
-
 function App() {
   const [questions, setQuestions] = useState(quiz);
   const [currentQuestionId, setCurrentQuestion] = useState(1);
